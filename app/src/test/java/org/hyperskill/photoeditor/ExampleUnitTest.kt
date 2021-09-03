6455cf43dfe83071bfb7777fd14a457cbc8172fd
NeedThisAccount <keruk33@gmail.com>
2021-09-03 14:59:18 +0200

    some tests

--- app/src/test/java/org/hyperskill/photoeditor/ExampleUnitTest.kt
+++ app/src/test/java/org/hyperskill/photoeditor/ExampleUnitTest.kt
@@ -1,12 +1,9 @@
 package org.hyperskill.photoeditor
 
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.result.ActivityResultRegistry
-import androidx.activity.result.contract.ActivityResultContract
-import androidx.core.app.ActivityOptionsCompat
+import android.widget.TextView
+import com.google.android.material.slider.Slider
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -45,6 +42,44 @@ class ExampleUnitTest {
 
         val message = "does view with id \"btnGalllery\" placed in activity?"
         assertNotNull(message, btnGallery)
+    }
+
+    @Test
+    fun testShouldCheckSliderExist() {
+        val activity = activityController.setup().get()
+        val slBrightness = activity.findViewById<Slider>(R.id.slBrightness)
+
+        val message = "does view with id \"slBrightness\" placed in activity?"
+        assertNotNull(message, slBrightness)
+
+        val message2 = "\"slider\" should have proper stepSize attribute"
+        assertEquals(message2, slBrightness.stepSize, 0.1f)
+
+        val message3 = "\"slider\" should have proper valueFrom attribute"
+        assertEquals(message3, slBrightness.valueFrom, 0.2f)
+
+        val message4 = "\"slider\" should have proper valueTo attribute"
+        assertEquals(message4, slBrightness.valueTo, 1.8f)
+
+        val message5 = "\"slider\" should have proper initial value"
+        assertEquals(message5, 1.0f, slBrightness.value)
+    }
+
+    @Test
+    fun testShouldCheckSliderWorkingWithoutImage() {
+        val activity = activityController.setup().get()
+        val slBrightness = activity.findViewById<Slider>(R.id.slBrightness)
+        val ivPhoto = activity.findViewById<ImageView>(R.id.ivPhoto)
+
+        val message = "does view with id \"slBrightness\" placed in activity?"
+        assertNotNull(message, slBrightness)
+
+        slBrightness.value = 1.2f
+
+        val drawable = (ivPhoto.drawable)
+        val message2 = "is \"ivPhoto\" empty and no crash occurs while swiping slider?"
+        assertNull(message2, drawable)
+
     }
 
     @Test
