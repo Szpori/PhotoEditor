896cc48645b8b89ffb6882251dc6bc74811c972b
NeedThisAccount <keruk33@gmail.com>
2021-09-07 17:28:48 +0200

    tests..

--- app/src/androidTest/java/org/hyperskill/photoeditor/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/hyperskill/photoeditor/ExampleInstrumentedTest.kt
@@ -1,24 +1,82 @@
-package org.hyperskill.photoeditor
-
+import android.app.Activity.RESULT_OK
+import android.app.Instrumentation.ActivityResult
+import android.content.ContentResolver
+import android.content.Intent
+import android.content.res.Resources
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.not
+import org.hyperskill.photoeditor.ImageViewHasDrawableMatcher.hasDrawable
+import org.hyperskill.photoeditor.MainActivity
+import org.hyperskill.photoeditor.R
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest{
+
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.hyperskill.photoeditor", appContext.packageName)
+    fun  test_validateIntentSentToPickPackage() {
+
+        // GIVEN
+        val expectedIntent: Matcher<Intent> = allOf(
+            hasAction(Intent.ACTION_PICK),
+            hasData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        )
+        val activityResult = createGalleryPickActivityResultStub()
+        intending(expectedIntent).respondWith(activityResult)
+
+        // Execute and Verify
+        onView(withId(R.id.btnGallery)).perform(click())
+        intended(expectedIntent)
+    }
+
+    private fun createGalleryPickActivityResultStub(): ActivityResult {
+        val resources: Resources = InstrumentationRegistry.getInstrumentation().context.resources
+        val imageUri = Uri.parse(
+            ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
+                    resources.getResourcePackageName(R.drawable.ic_launcher_background) + '/' +
+                    resources.getResourceTypeName(R.drawable.ic_launcher_background) + '/' +
+                    resources.getResourceEntryName(R.drawable.ic_launcher_background)
+        )
+        val resultIntent = Intent()
+        resultIntent.setData(imageUri)
+        return ActivityResult(RESULT_OK, resultIntent)
+    }
+
+    @Test
+    fun test_picture_set() {
+
+        // GIVEN
+        val expectedIntent: Matcher<Intent> = allOf(
+            hasAction(Intent.ACTION_PICK),
+            hasData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        )
+        val activityResult = createGalleryPickActivityResultStub()
+        intending(expectedIntent).respondWith(activityResult)
+
+        // Execute and Verify
+        onView(withId(R.id.ivPhoto)).check(matches(not(hasDrawable())))
+        onView(withId(R.id.btnGallery)).perform(click())
+        intended(expectedIntent)
+        onView(withId(R.id.ivPhoto)).check(matches(hasDrawable()))
     }
 }
